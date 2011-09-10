8074d641bbeabd1f900c7748b51b2d9f7702ed6e
Loren Norman <lorennorman@gmail.com>
2011-09-10 10:56:36 -0400

    Rails 3.1 app generated.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MobileJam::Application.config.secret_token = 'f1d01b63ed92d40ed6296b577c4df39007e3bd7acf7dffa5b75acf052c6f011c22739aab684f5fe5a85062600d545765d1d915fa66e14beea0ea2bd9568202ac'
